bd4770a1af30b79dab1ba40fb76cfa1af3d27173
KubsterPlayer <62438030+KubsterPlayer@users.noreply.github.com>
2022-09-30 13:41:14 +0200

    Fixed error handling
    ok

--- main.py
+++ main.py
@@ -7,25 +7,22 @@ import ascii
 
 ascii.titlescreen()
 
-print(Fore.BLUE + "Please input your youtube link below:")
-link = input(Fore.RED + ">")
-print("")
-
 while True:
     try:
-        
+        print(Fore.BLUE + "Please input your youtube link below:")
+        link = input(Fore.RED + ">")
+        print("")      
         yt = YouTube(link)
-        break
+
+
+        ys = yt.streams.get_highest_resolution()
+
+        print(Fore.BLUE + "Video title: " + Fore.RED + yt.title)
+        print(Fore.BLUE + "Author " + Fore.RED + yt.author)
+        print(Fore.BLUE + "Resolution: " + Fore.RED + ys.resolution)
+        print(Fore.BLUE + "Video lengh: " + Fore.RED + str(yt.length) + Fore.BLUE + " seconds.")
+
+        ys.download("")
+        print(Fore.RED + ("download has been completed"))
     except:
-        continue
-
-
-ys = yt.streams.get_highest_resolution()
-
-print(Fore.BLUE + "Video title: " + Fore.RED + yt.title)
-print(Fore.BLUE + "Author " + Fore.RED + yt.author)
-print(Fore.BLUE + "Resolution: " + Fore.RED + ys.resolution)
-print(Fore.BLUE + "Video lengh: " + Fore.RED + str(yt.length) + Fore.BLUE + " seconds.")
-
-ys.download("")
-print(Fore.RED + ("download has been completed"))
+        print("Please enter a valid youtube link")
